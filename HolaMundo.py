f9b3314f992ee75bf60bc41d175b0e287e8f83e9
mez042 <mgomezj2301@alumno.ipn.mx>
2024-10-21 18:00:47 -0600

    01Ejemplo.py

---
+++ HolaMundo.py
@@ -0,0 +1,39 @@
+#esto es un comentario
+
+#vamos a definir una variable
+
+#ejemplo de if una condicion
+
+#x = 28
+
+#if x < 0 in valores:
+    #cuando sea real
+ #   pring('Es menor a 0')
+#elif x > 0 :
+  #  pring('Es mayor a 0')
+
+#else:
+ #   pring('Es 0')
+
+
+#Bucles
+#numero = 0
+#pring("Tabla del 2")
+
+#while numero <= 0 :
+ #   pring("Resultado : , 2*numero")
+  #  pring("Antes" , numero)
+   # numero = 1
+#    pring("Despues: " , numero)
+
+
+#Bucle for
+
+numero : [3, 7, 5, 8]
+
+for n in numero :
+    pring(n)
+    
+    
+    
+    print("hola Mundo")

f2b1b397dae04c87162cd2a103bae72fbf0b7dfd
mez042 <mgomezj2301@alumno.ipn.mx>
2024-10-21 18:01:25 -0600

    02Ejemplo.py

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+#vamos a crear una calculadora sencilla
+#para la entrada de datos es necesario utilizar el metodo
+
+numero1 = float(input("Introduce el primer numero : "))
+numero2 = float(input("Introduce el segundo numero : "))
+
+#vaos a sumar
+
+suma = numero1 + numero02
+
+pring("La  suma es de:" , suma)
